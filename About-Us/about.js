c25a615185ab492aaea4c575e5276d6a64d5adfd
KenjiArdianto <151555379+KenjiArdianto@users.noreply.github.com>
2024-05-01 15:29:14 +0700

    Update about.js

--- About-Us/about.js
+++ About-Us/about.js
@@ -1,16 +1,33 @@
 // Navbar
 const userclick = document.getElementById('user-click');
+const arrowclick = document.getElementById('arrow-click');
 const subMenu = document.getElementById('acc-menu');
 
 let isSubMenuOpen = false;
+
 
 userclick.addEventListener('click', () => {
     if (!isSubMenuOpen) {
         subMenu.style.transition = '1s';
         subMenu.style.height = '150px';
+        arrowclick.style.transform = "rotate(180deg)";
     } else {
         subMenu.style.transition = '0.5s';
         subMenu.style.height = '0';
+        arrowclick.style.transform = "rotate(0deg)";
+    }
+    isSubMenuOpen = !isSubMenuOpen;
+});
+
+arrowclick.addEventListener('click', () => {
+    if (!isSubMenuOpen) {
+        subMenu.style.transition = '1s';
+        subMenu.style.height = '150px';
+        arrowclick.style.transform = "rotate(180deg)";
+    } else {
+        subMenu.style.transition = '0.5s';
+        subMenu.style.height = '0';
+        arrowclick.style.transform = "rotate(0deg)";
     }
     isSubMenuOpen = !isSubMenuOpen;
 });
